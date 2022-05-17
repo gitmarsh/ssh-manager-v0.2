30c8413d6c7b89207c87308bd744004f64430aca
Marshall Hearn <93810451+gitmarsh@users.noreply.github.com>
2022-05-17 15:30:42 -0500

    Update ssh-manager.sh
    updated

--- ssh-manager.sh
+++ ssh-manager.sh
@@ -10,46 +10,39 @@ cyan="\e[36m"
 grey="\e[37m"
 reset="\e[0m"
 
-
 function newserver () {
+
 read -p $'\e[35mEnter server ip\e[0m: ' serverip
 read -p  $'\e[34mEnter username\e[0m: ' user
 read -p $'\e[36mEnter an  alias\e[0m: ' nickname
 sleep 1
-newid="$user@$serverip"
+newid="$($user@$serverip)"
 fileid=$RANDOM
-  echo "$newid" > ./server_list/"$fileid".conf &&
-	gpg -c ./server_list/"$fileid".conf &&		## Decrypts server list and adds new key	
-	rm ./server_list/"$fileid".conf &&		## Encrypts the updated list and deletes the old list
-	gpg -d identity.conf.gpg > identity.conf &&	## Decrypts server identitifier 
-	gpg -d serverlist.gpg > serverlist &&		## Encrypts the updated list and deletes the old list
-  echo "$nickname" >> serverlist &&
-	gpg -c serverlist && rm serverlist &&
-  echo "$nickname= $fileid" >> identity.conf &&
-	rm identity.conf.gpg &&
-	gpg -c identity.conf && 
-	rm identity.conf
-	
+
+echo "$newid" > './server_list/"$fileid".conf' &&
+		gpg -c './server_list/"$fileid".conf' &&
+	rm './server_list/"$fileid".conf' &&
+		gpg -d 'identity.conf.gpg' > 'identity.conf' &&
+		gpg -d 'serverlist.gpg' > 'serverlist' &&
+echo "$nickname" >> 'serverlist' &&
+		gpg -c 'serverlist' && rm 'serverlist' &&
+echo "$nickname"= "$fileid" >> 'identity.conf' &&
+	rm 'identity.conf.gpg' &&
+		gpg -c 'identity.conf' && 
+	rm 'identity.conf'
 echo -e "${green}Quick SSH Entry Added!${reset}"
-echo "ssh $user@$serverip"  >> ~/.scripts/sshkeymanager/server_aliases/"$nickname($serverip)"
-chmod +x ~/.scripts/sshkeymanager/server_aliases/"$nickname($serverip)"
-sleep 1
-echo -e "${cyan}Connect to the new server${reset}: (y)yes/(n)no"
-read -n 1 -s answer 
-if [ "$answer" == "y" ]
-then
-	sh ~/.scripts/sshkeymanager/server_aliases/"$nickname($serverip)"
-else
-	sleep 1 && echo "SSH Connect Closing..." && sleep 2
-    exit
-fi
+
+exit
+
 }
 
 function connect () {
+
 gpg -d serverlist.gpg > .srv.tmp && cat .srv.tmp -n
 
 echo -e "\n${cyan}Enter server name you wish to connect to${reset}\n"
-	read -n 1 -s answer2
+read -n 1 -s answer2
+
 tempid="$(cat .srv.tmp | sed -n "$answer2"p | awk "{print $1}" )"
 serverid="$(gpg -d identity.conf.gpg | grep -e "$tempid" | awk -F"=" '{print $2}')"
 connectid="$(gpg -d ./server_list/$serverid.conf.gpg)"
@@ -58,19 +51,19 @@ sleep 1
 echo -e "${yellow}Connecting.....${reset} ${purple}$serverid${reset}\n"
 sleep 1
 rm .srv.tmp && ssh "$connectid"
+
 }
 
 function delete () {
-	ls -1 ~/.scripts/sshkeymanager/server_aliases/ | cat -n
+ls -1 ~/.scripts/sshkeymanager/server_aliases/ | cat -n
+
 echo -e "\n${cyan}Enter server name you wish to delete${reset}\n"
 	read -n 1 -s answer2
 	
 serverid="$( ls -1 ~/.scripts/sshkeymanager/server_aliases/ | sed -n "$answer2"p | awk "{print $1}" )"
 sleep 1
-
 echo -e "${red}Are you sure you wish to delete this server from this list?${reset}: (y)Yes/(n)No"
-	read -n 1 -s confirm
-
+read -n 1 -s confirm
 if [ "$confirm" == "y" ]
 then
 rm ~/.scripts/sshkeymanager/server_aliases/"$serverid"
@@ -100,11 +93,11 @@ echo -e "${green}#########################${reset} ${red}SSH QUICK CONNECT / SER
 
 ###  MENU
 
-function menu () { 
-echo -e "\n${cyan}Select an option${reset}:\n\n1.) ${green}Connect To Server${reset}  2.) ${purple}List Servers${reset}   3.) ${yellow}Add New Server${reset}   4.) ${red}Delete Server${reset}   5.) Exit\n"
-read -n 1 -s firstab
-sleep 1 
-case $firstab in
+    function menu () { 
+        echo -e "\n${cyan}Select an option${reset}:\n\n1.) ${green}Connect To Server${reset}  2.) ${purple}List Servers${reset}   3.) ${yellow}Add New Server${reset}   4.) ${red}Delete Server${reset}   5.) Exit\n"
+        read -n 1 -s firstab
+        sleep 1 
+        case $firstab in
         "1")
         connect     ;;
         "2")
